2a0210c84ca11f2ab502e74dbb590dd3e4d36747
CIS\m.houska <m.houska@creditinfosolutions.com>
2017-07-28 18:01:09 +0200

    100% done, extra_credit_task remains to be solved

--- python3/koans/about_regex.py
+++ python3/koans/about_regex.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
+
 from runner.koan import *
 import re
 class AboutRegex(Koan):
@@ -15,7 +17,7 @@ class AboutRegex(Koan):
             Lesson 1 Matching Literal String
         """
         string = "Hello, my name is Felix and this koans are based on the Ben's book: Regular Expressions in 10 minutes."
-        m = re.search(__, string)
+        m = re.search("Felix", string)
         self.assertTrue(m and m.group(0) and m.group(0)== 'Felix', "I want my name")
 
     def test_matching_literal_text_how_many(self):
@@ -32,10 +34,10 @@ class AboutRegex(Koan):
 
         """
         string = "Hello, my name is Felix and this koans are based on the Ben's book: Regular Expressions in 10 minutes. Repeat My name is Felix"
-        m = re.match('Felix', string) #TIP: Maybe match it's not the best option
+        m = re.findall('Felix', string) #TIP: Maybe match it's not the best option
 
         # I want to know how many times appears my name
-        self.assertEqual(m, __)
+        self.assertEqual(len(m), 2)
 
     def test_matching_literal_text_not_case_sensitivity(self):
         """
@@ -47,8 +49,8 @@ class AboutRegex(Koan):
         """
         string = "Hello, my name is Felix or felix and this koans is based on the Ben's book: Regular Expressions in 10 minutes."
 
-        self.assertEqual(re.findall("felix", string), __)
-        self.assertEqual(re.findall("felix", string, re.IGNORECASE), __)
+        self.assertEqual(re.findall("felix", string), ["felix"])
+        self.assertEqual(re.findall("felix", string, re.IGNORECASE), ["Felix", "felix"])
 
     def test_matching_any_character(self):
         """
@@ -65,7 +67,7 @@ class AboutRegex(Koan):
 
         # TIP: remember the name of this lesson
 
-        change_this_search_string = 'a..xlx' # <-- I want to find all uses of myArray
+        change_this_search_string = 'a..xls' # <-- I want to find all uses of myArray
         self.assertEquals(len(re.findall(change_this_search_string, string)),3)
 
     def test_matching_set_character(self):
@@ -86,7 +88,7 @@ class AboutRegex(Koan):
                 + "ca1.xls"
         # I want to find all files for North America(na) or South America(sa), but not (ca)
         # TIP you can use the pattern .a. which matches in above test but in this case matches more than you want
-        change_this_search_string = '[nsc]a[2-9].xls'
+        change_this_search_string = '[ns]a[1-9].xls'
         self.assertEquals(len(re.findall(change_this_search_string, string)),3)
 
     def test_anything_but_matching(self):
@@ -110,7 +112,7 @@ class AboutRegex(Koan):
                 + "ca1.xls"
 
         # I want to find the name sam
-        change_this_search_string = '[^nc]am'
+        change_this_search_string = '[^nc]am.xls'
         self.assertEquals(re.findall(change_this_search_string, string), ['sam.xls'])
 
 
